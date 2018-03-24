3eecce70ca45c325e3d6e388756bcae8a12c09a5
florentw <florent.weber@gmail.com>
2018-03-24 14:51:09 +0100

    Remove a bit of duplication in tests.

--- game/test_game.py
+++ game/test_game.py
@@ -12,10 +12,10 @@ class TestGame(TestCase):
         self.game.close()
 
     def test_join_inserts_a_new_user(self):
+
         (inserted, msg) = self.game.join("U1", "User1")
 
-        self.assertTrue(inserted)
-        self.assertTrue("you are now registered" in msg)
+        self.assert_success(inserted, msg, "you are now registered")
         (status, scores) = self.game.list_high_scores("", "")
         self.assertTrue("U1" in scores)
 
@@ -24,36 +24,31 @@ class TestGame(TestCase):
 
         (inserted, msg) = self.game.join("U1", "User1")
 
-        self.assertFalse(inserted)
-        self.assertTrue("you are already registered" in msg)
+        self.assert_error(inserted, msg, "you are already registered")
 
     def test_join_with_invalid_username_returns_false(self):
         (inserted, msg) = self.game.join("U1", "")
 
-        self.assertFalse(inserted)
-        self.assertTrue("you have to provide a valid user name" in msg)
+        self.assert_error(inserted, msg, "you have to provide a valid user name")
 
     def test_join_with_same_name_as_another_player_returns_false(self):
         self.game.join("U1", "User1")
 
         (inserted, msg) = self.game.join("U2", "User1")
 
-        self.assertFalse(inserted)
-        self.assertTrue("someone is already registered with that name" in msg)
+        self.assert_error(inserted, msg, "someone is already registered with that name")
 
     def test_leave_removes_previously_registered_player(self):
         self.game.join("U1", "User1")
 
         (removed, msg) = self.game.leave("U1")
 
-        self.assertTrue(removed)
-        self.assertTrue("you are now unregistered" in msg)
+        self.assert_success(removed, msg, "you are now unregistered")
 
     def test_leave_returns_false_for_unknown_player(self):
         (removed, msg) = self.game.leave("U1")
 
-        self.assertFalse(removed)
-        self.assertTrue("register first" in msg)
+        self.assert_error(removed, msg, "register first")
 
     def test_commands_returns_commands_dict(self):
         commands = self.game.commands()
@@ -66,123 +61,101 @@ class TestGame(TestCase):
     def test_add_task_returns_false_if_player_not_registered(self):
         (inserted, msg) = self.game.add_task("unknown", "1 task")
 
-        self.assertFalse(inserted)
-        self.assertTrue("you have to register first" in msg)
+        self.assert_have_to_register(msg, inserted)
 
     def test_add_task_returns_false_if_wrong_format_for_points(self):
         self.game.join("U1", "User1")
 
         (inserted, msg) = self.game.add_task("U1", "points task")
 
-        self.assertFalse(inserted)
-        self.assertTrue("invalid format for points" in msg)
+        self.assert_error(inserted, msg, "invalid format for points")
 
     def test_add_task_returns_false_if_empty_argument(self):
         self.game.join("U1", "User1")
 
         (inserted, msg) = self.game.add_task("U1", "")
 
-        self.assertFalse(inserted)
-        self.assertTrue("invalid arguments" in msg)
+        self.assert_error(inserted, msg, "invalid arguments")
 
     def test_take_added_task_returns_false_when_negative_points(self):
         self.game.join("U1", "User1")
 
         (inserted, msg) = self.game.add_task("U1", "-12 task")
 
-        self.assertFalse(inserted)
-        self.assertTrue("points must be between 1 and 42 included" in msg)
+        self.assert_error(inserted, msg, "points must be between 1 and 42 included")
 
     def test_take_added_task_returns_false_when_points_too_high(self):
         self.game.join("U1", "User1")
 
         (inserted, msg) = self.game.add_task("U1", "1337 task")
 
-        self.assertFalse(inserted)
-        self.assertTrue("points must be between 1 and 42 included" in msg)
+        self.assert_error(inserted, msg, "points must be between 1 and 42 included")
 
     def test_add_task_returns_task_id_when_inserted(self):
         self.game.join("U1", "User1")
 
         (inserted, msg) = self.game.add_task("U1", "3 New task")
 
-        self.assertTrue(inserted)
-        self.assertTrue("added with id *1* for *3* point(s)" in msg)
+        self.assert_success(inserted, msg, "added with id *1* for *3* point(s)")
 
     def test_take_added_task_returns_true(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.take_task("U1", "1")
 
-        self.assertTrue(status)
-        self.assertTrue("you are taking ownership of *New task* for 3 point(s)" in msg)
+        self.assert_success(status, msg, "you are taking ownership of *New task* for 3 point(s)")
 
     def test_take_unknown_task_id_returns_false(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.take_task("U1", "2")
 
-        self.assertFalse(status)
-        self.assertTrue("this task does not exist" in msg)
+        self.assert_task_does_not_exist(msg, status)
 
     def test_take_task_returns_false_if_player_not_registered(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.take_task("U2", "1")
 
-        self.assertFalse(status)
-        self.assertTrue("you have to register first" in msg)
+        self.assert_have_to_register(msg, status)
 
     def test_take_invalid_task_id_returns_false(self):
         self.game.join("U1", "User1")
 
         (status, msg) = self.game.take_task("U1", "")
 
-        self.assertFalse(status)
-        self.assertTrue("invalid task id" in msg)
+        self.assert_error(status, msg, "invalid task id")
 
     def test_drop_task_returns_false_if_player_not_registered(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.drop_task("U2", "1")
 
-        self.assertFalse(status)
-        self.assertTrue("you have to register first" in msg)
+        self.assert_have_to_register(msg, status)
 
     def test_drop_task_with_invalid_task_id_returns_false(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.drop_task("U1", "")
 
-        self.assertFalse(status)
-        self.assertTrue("invalid task id" in msg)
+        self.assert_error(status, msg, "invalid task id")
 
     def test_drop_task_with_unknown_task_id_returns_false(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.drop_task("U1", "2")
 
-        self.assertFalse(status)
-        self.assertTrue("this task does not exist" in msg)
+        self.assert_task_does_not_exist(msg, status)
 
     def test_drop_task_not_assigned_to_player_returns_false(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.drop_task("U1", "1")
 
-        self.assertFalse(status)
-        self.assertTrue("you are not assigned to this task" in msg)
+        self.assert_error(status, msg, "you are not assigned to this task")
 
     def test_drop_task_returns_true_when_successful(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
         self.game.take_task("U1", "1")
 
         (status, msg) = self.game.drop_task("U1", "1")
@@ -193,50 +166,40 @@ class TestGame(TestCase):
         self.assertFalse(status)
 
     def test_close_task_returns_false_if_player_not_registered(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.close_task("U2", "1")
 
-        self.assertFalse(status)
-        self.assertTrue("you have to register first" in msg)
+        self.assert_have_to_register(msg, status)
 
     def test_close_task_with_invalid_task_id_returns_false(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.close_task("U1", "")
 
-        self.assertFalse(status)
-        self.assertTrue("invalid task id" in msg)
+        self.assert_error(status, msg, "invalid task id")
 
     def test_close_task_with_unknown_task_id_returns_false(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.close_task("U1", "2")
 
-        self.assertFalse(status)
-        self.assertTrue("this task does not exist" in msg)
+        self.assert_task_does_not_exist(msg, status)
 
     def test_drop_task_not_assigned_to_player_returns_true(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.close_task("U1", "1")
 
-        self.assertTrue(status)
-        self.assertTrue("the task *1*, *New task* has been closed by *User1*" in msg)
+        self.assert_success(status, msg, "the task *1*, *New task* has been closed by *User1*")
 
     def test_drop_task_assigned_to_player_returns_true(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
         self.game.take_task("U1", "1")
 
         (status, msg) = self.game.close_task("U1", "1")
 
-        self.assertTrue(status)
-        self.assertTrue("the task *1*, *New task* has been closed by *User1*" in msg)
+        self.assert_success(status, msg, "the task *1*, *New task* has been closed by *User1*")
 
     def test_list_tasks_returns_true_when_no_task(self):
         (status, msg) = self.game.list_tasks()
@@ -279,50 +242,40 @@ class TestGame(TestCase):
         self.assertTrue(expected in msg)
 
     def test_assign_with_weighted_random_return_false_when_user_not_registered(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.assign_with_weighted_random("U2", "1")
 
-        self.assertFalse(status)
-        self.assertTrue("you have to register first" in msg)
+        self.assert_have_to_register(msg, status)
 
     def test_assign_with_weighted_random_return_false_when_task_id_does_not_exist(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.assign_with_weighted_random("U1", "2")
 
-        self.assertFalse(status)
-        self.assertTrue("this task does not exist" in msg)
+        self.assert_task_does_not_exist(msg, status)
 
     def test_assign_with_weighted_random_return_false_when_task_id_invalid(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.assign_with_weighted_random("U1", "garbage")
 
-        self.assertFalse(status)
-        self.assertTrue("invalid task id" in msg)
+        self.assert_error(status, msg, "invalid task id")
 
     def test_assign_with_weighted_random_an_already_assigned_task_returns_false(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
         self.game.take_task("U1", "1")
 
         (status, msg) = self.game.assign_with_weighted_random("U1", "1")
 
-        self.assertFalse(status)
-        self.assertTrue("a player is already assigned to this task" in msg)
+        self.assert_error(status, msg, "a player is already assigned to this task")
 
     def test_assign_with_weighted_random_assigns_task_to_unique_player(self):
-        self.game.join("U1", "User1")
-        self.game.add_task("U1", "3 New task")
+        self.join_and_add_task()
 
         (status, msg) = self.game.assign_with_weighted_random("U1", "1")
 
-        self.assertTrue(status)
-        self.assertTrue("The universe has spoken, congrats <@U1>" in msg)
+        self.assert_success(status, msg, "The universe has spoken, congrats <@U1>")
 
     def test_assign_with_weighted_random_assigns_task_to_of_two_players(self):
         self.game.join("U1", "User1")
@@ -343,6 +296,26 @@ class TestGame(TestCase):
         for command, (function, description) in self.game.commands().items():
             self.assertTrue(command in help_output)
             self.assertTrue(description in help_output)
+
+    def assert_error(self, status, msg, expected_msg):
+        self.assertFalse(status)
+        self.assertTrue(expected_msg in msg)
+
+    def assert_success(self, status, msg, expected_msg):
+        self.assertTrue(status)
+        self.assertTrue(expected_msg in msg)
+
+    def assert_have_to_register(self, msg, status):
+        self.assertFalse(status)
+        self.assertTrue("you have to register first" in msg)
+
+    def join_and_add_task(self):
+        self.game.join("U1", "User1")
+        self.game.add_task("U1", "3 New task")
+
+    def assert_task_does_not_exist(self, msg, status):
+        self.assertFalse(status)
+        self.assertTrue("this task does not exist" in msg)
 
     def populate_tasks_list_and_assignments(self):
         self.game.join("U1", "User1")
